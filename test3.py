b82a30abb42bae4fa5943c81f80acecc16fef634
tobi o <tobystic@gmail.com>
2020-02-14 07:42:50 -0800

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,7 +1,7 @@
 
 ### test to reverse a string, can be called if equal to return boolean true. if cw===wc
 
-wordtotest = input("type a string : ")
+wordtotest = input("type a word(string) to get it's reversed version : ")
 wordchecker = str(wordtotest)
 
 wc = wordchecker[0: len(wordtotest)]
