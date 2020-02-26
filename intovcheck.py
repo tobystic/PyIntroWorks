c25aa8f56c9ec924902b247796559dc831a329f7
tobi o <tobystic@gmail.com>
2020-02-26 08:55:44 -0800

    Update intovcheck.py
    floating integer point python

--- intovcheck.py
+++ intovcheck.py
@@ -2,4 +2,5 @@ class intovcheck:
 
     x1 = int(input("enter a value : "))
     x2 = int(input("enter a value : "))
+    #check for floating integer point in python 
     print(f"x1 divided by x2 is: {x1/x2 :20f}")
