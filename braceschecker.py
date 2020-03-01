8d0af833bd9e35d9fe066f208cc0dce51b882155
tobi o <tobystic@gmail.com>
2020-02-29 20:17:56 -0800

    Update braceschecker.py
    checking first and last braces

--- braceschecker.py
+++ braceschecker.py
@@ -11,7 +11,11 @@ def braceschecker(userword):
         if wordchecker[i] == ")":
             counter2 += 1
     print(counter1, counter2)
-
+    #check the follwong next 3 lines to be sure it works
+    if wordchecker[1] ==")" or wordchecker[(len(wordchecker) - 1)] =="(":
+        return("Inapproriate start or ending tag, check again")
+        break
+    
     if counter1 == counter2:
         print("They are equal")
     else:
@@ -20,3 +24,5 @@ def braceschecker(userword):
 
 woftd = "today ( is the ( day )) that (( the Lord has ) made"
 braceschecker(woftd)
+
+#additional errors check 1. final brace cannot be "(" and first brace cannot be ")"
