32feab1e35d85fff22dc63006cdea9678d525b8c
tobystic <tobystic@gmail.com>
2020-03-12 01:47:01 -0700

    usecleaning .

---
+++ milotest.py
@@ -0,0 +1,17 @@
+
+
+sentence = "TACGCTTGACAAGACGTCTCCATGTCCTTTCAAGCGACTTGCTACGCTATGCACCATGC"
+print("The number of times ATG occurs is: " + str(sentence.count("ATG")))
+
+
+
+'''sentence = input("Type your word: ")
+y = "ATG"
+searchstart = 0
+for i in range(len(sentence)):
+    count = 0
+    if y in sentence and i < len(sentence):
+        count += 1
+        searchstart = i
+
+print(count)'''
